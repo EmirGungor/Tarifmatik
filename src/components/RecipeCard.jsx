c054feb854076c778bad064e30cb0521bd1359c6
EmirGungor <emirgungor07@hotmail.com>
2025-04-19 22:33:20 +0300

    css dosyası açıldı

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
+import '../styles/recipecard.css'
 
 const RecipeCard = ({ title, content, onAlternativeClick }) => {
   return (
-    <div style={{
-      border: '1px solid #ddd',
-      borderRadius: '12px',
-      padding: '16px',
-      marginBottom: '16px',
-      boxShadow: '0px 4px 10px rgba(0,0,0,0.1)',
-      backgroundColor: '#fff'
-    }}>
+    <div className='rcp-card'>
       <h2 style={{ marginBottom: '8px' }}>🍽️ {title}</h2>
       <p style={{ marginBottom: '12px' }}>{content}</p>
-      <button onClick={onAlternativeClick} style={{
-        backgroundColor: '#ff9800',
-        color: '#fff',
-        border: 'none',
-        padding: '8px 12px',
-        borderRadius: '8px',
-        cursor: 'pointer'
-      }}>
+      <button onClick={onAlternativeClick} className='rcp-btn'>
         Alternatif Tarifler
       </button>
     </div>
